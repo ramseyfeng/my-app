02365e03eeed357b161ec62a8a1a11081d42045a
Ramsey Feng <huan.feng@microfocus.com>
2019-04-01 17:27:07 +0800

    Updates

---
+++ precache-manifest.ced0c9a5fdb47e086f758c3cb15e2ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "f504241b69519b6d7307",
+    "url": "/my-app/static/js/main.abe4d684.chunk.js"
+  },
+  {
+    "revision": "f498584d6e5715d33675",
+    "url": "/my-app/static/js/2.db077af6.chunk.js"
+  },
+  {
+    "revision": "f498584d6e5715d33675",
+    "url": "/my-app/static/css/2.717437d3.chunk.css"
+  },
+  {
+    "revision": "7c86cbd083467a1f680a1d16639a5deb",
+    "url": "/my-app/index.html"
+  }
+];
